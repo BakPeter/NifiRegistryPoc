5d994c871ebc58a0f358875cd4794fcc4499c3a6
Peter Bakshaev <bak.pet@gmail.com>
2023-10-25 13:20:18 +0300

    WIP - nifi- management-server

--- nifi-management-server/index.js
+++ nifi-management-server/index.js
@@ -1,71 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
+const azios = require('axios');
 
-const settings = require('./app-settings.json');
+require('dotenv').config();
 
-console.log(`settings: ${JSON.stringify(settings)}`);
+const settings = {
+  serverName: process.env.SERVER_NAME,
+  serverPort: process.env.SERVER_PORT,
+};
+
+console.log('NifiManagementServer started');
 
 const app = express();
 app.use(bodyParser.json());
 
-// rnd
-// ===========================================================================================
-// ===========================================================================================
-app.get('/rnd/about', async (req, res) => {
-  //   axios
-  //     .get('http://localhost:18080/nifi-registry-api/about')
-  //     .then((res) => {})
-  //     .catch((error) => {});
-
-  try {
-    const response = await axios.get(
-      'http://localhost:18080/nifi-registry-api/about'
-    );
-    res.status(200).send(response.data);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+app.get('/helth', (req, res) => {
+  res.status(200).send({ processConfigurations: settings });
 });
 
-app.get('/rnd/buckets', async (req, res) => {
-  try {
-    const response = await axios.get(
-      'http://localhost:18080/nifi-registry-api/buckets'
-    );
-    res.status(200).send(response.data);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-app.post('/rnd/buckets', async (req, res) => {
-  try {
-    const { description, name } = req.body;
-
-    const response = await axios.post(
-      'http://localhost:18080/nifi-registry-api/buckets',
-      { description, name }
-    );
-    res.status(200).send(response.data);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
-app.get('/rnd/export/:flowId', (req, res) => {
-  const flowId = req.params.flowId;
-  const { version } = req.body;
-  const reqData = { flowId, version };
-
-  console.log(`${JSON.stringify(reqData)}`);
-
-  res.status(200).send({ reqData });
-});
-
-// ===========================================================================================
-// ===========================================================================================
-
-app.listen(settings.port, () => {
-  console.log(`Listening on port ${settings.port}`);
+app.listen(settings.serverPort, () => {
+  console.log(
+    `${settings.serverName} listenning on port ${settings.serverPort}`
+  );
 });
